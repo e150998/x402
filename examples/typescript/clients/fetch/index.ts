b139d2735bb0b79c64aafa75347f5b6e8f073800
Harsh Bishnoi <harsh.bishnoi@mastercard.com>
2025-08-21 11:40:19 -0400

    debugging signature

--- examples/typescript/clients/fetch/index.ts
+++ examples/typescript/clients/fetch/index.ts
@@ -1,8 +1,7 @@
 import { config } from "dotenv";
-import { Hex } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
+import { Hex} from "viem";
 import { decodeXPaymentResponse, wrapFetchWithPayment } from "x402-fetch";
-
+import { LocalAccountSigner } from "@aa-sdk/core";
 config();
 
 const privateKey = process.env.PRIVATE_KEY as Hex;
@@ -15,20 +14,25 @@ if (!baseURL || !privateKey || !endpointPath) {
   process.exit(1);
 }
 
-const account = privateKeyToAccount(privateKey);
 
-const fetchWithPayment = wrapFetchWithPayment(fetch, account);
+const account = LocalAccountSigner.privateKeyToAccountSigner(privateKey);
+
+console.log("Using account:", account, typeof account);
+
+
+const fetchWithPayment = wrapFetchWithPayment(fetch, account.inner);
 
 fetchWithPayment(url, {
   method: "GET",
 })
   .then(async response => {
     const body = await response.json();
-    console.log(body);
+    console.log("body",body);
 
     const paymentResponse = decodeXPaymentResponse(response.headers.get("x-payment-response")!);
     console.log(paymentResponse);
   })
   .catch(error => {
+    // console.error("Error during fetch with payment:", error);
     console.error(error.response?.data?.error);
   });
